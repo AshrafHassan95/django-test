e1b5095f51bcefe43371370286ab5126387a40b7
AshrafHassan95 <ashraf950901@gmail.com>
2024-05-06 05:01:20 +0800

    modified commit

--- movies/views.py
+++ movies/views.py
@@ -15,8 +15,35 @@ def movies(request):
 def home(request):
     return HttpResponse("Home page")
 
+@api_view(['GET', 'POST'])
+def movie_list(request):
+
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return JsonResponse({"movies":serializer.data})
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def detail(request, id):
-    data = Movie.objects.get(pk=id)
+
+    try:
+        data = Movie.objects.get(pk=id)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = MovieSerializer(movies)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
     return render(request, 'movies/detail.html', {'movie':data})
 
 def add(request):
@@ -36,16 +63,3 @@ def delete(request, id):
         raise Http404('Movie does not  exist')
     movies.delete()
     return HttpResponseRedirect('/movies')
-
-@api_view(['GET', 'POST'])
-def movie_list(request):
-
-    if request.method == 'GET':
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return JsonResponse({"movies":serializer.data})
-    if request.method == 'POST':
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
